ffd5efe175446c5b510c9869c57dffc6c44032e1
jayawickrma <142202862+jayawickrma@users.noreply.github.com>
2024-06-21 00:44:50 +0530

    day 2 & 3

--- src/main/java/lk/ijse/aad/Demo.java
+++ src/main/java/lk/ijse/aad/Demo.java
@@ -1,16 +1,30 @@
 package lk.ijse.aad;
 
+import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+
 import java.io.IOException;
 @WebServlet(value = "/health")
 public class Demo extends HttpServlet {
+    public void init(ServletConfig config)  {
+        System.out.println("server started");
+    }
+
+    @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         System.out.println("Nishan");
     }
 
+    protected void doHead(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("hhhah");
     }
+
+    public void destroy(){
+        System.out.println("bye byeee !!!!!!!");
+    }
+}
---
+++ src/main/java/lk/ijse/aad/student.java
@@ -0,0 +1,24 @@
+package lk.ijse.aad;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet(name ="/student")
+public class student extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("context path: "+req.getContextPath());
+        System.out.println("servelet path: "+req.getServletPath());
+        System.out.println("path info: "+req.getPathInfo());
+        System.out.println("path trancelated: "+req.getPathTranslated());
+        System.out.println("Query String: "+req.getQueryString());
+        System.out.println("request URI: "+req.getRequestURI());
+        System.out.println("request URL: "+req.getRequestURL());
+        System.out.println("get prptocol: "+req.getProtocol());
+        System.out.println("get scheema: "+req.getScheme());
+    }
+}
